417c198277c15da36e882aba6fd480be1cb3c635
LevPesin-Tech <78404922+LevPesin-Tech@users.noreply.github.com>
2021-11-16 23:58:13 -0600

    use new slider. update js and update the headers of every page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+/*slider box code*/
+$(document).ready(function(){
+  $('.slider').bxSlider({
+    captions: true
+  });
+});
 /*code for the sign up page*/
 document.getElementById("news-verification").onclick = function() {
     myFunction();
@@ -19,34 +25,3 @@ function submitFunction() {
   alert("The confirmation was sent to provided email address. Check your inbox and verify the email.");
 }
 /* end of code for the sign up page*/
-
-/* code for home page*/
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
-/*end of code for home page*/
